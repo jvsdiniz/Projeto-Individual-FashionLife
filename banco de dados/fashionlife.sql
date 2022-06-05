c4e4bae240c7d7154de6f29aa99185303657eade
jvsdiniz <joao.diniz@sptech.school>
2022-06-05 16:27:12 -0300

    API configurada e funcionando

--- banco de dados/fashionlife.sql
+++ banco de dados/fashionlife.sql
@@ -7,10 +7,11 @@ idUsuario int primary key auto_increment,
 nome varchar (45),
 email varchar (60),
 senha varchar (15),
-genero int
-);
+idade int,
+genero char(5),
+constraint chkTipo in(genero = 'Homem', 'Mulher');
 
--- create table (
+-- create table desfile (
 -- idVotacao int primary key auto_increment,
 
 -- fkVoto int,
